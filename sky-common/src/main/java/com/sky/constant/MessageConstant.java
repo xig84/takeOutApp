e56fbb4c355a00d4863cd995a8241a56f62a4eef
XIG <xig84@pitt.edu>
2024-01-30 14:26:08 -0500

    insert new employee information

--- sky-common/src/main/java/com/sky/constant/MessageConstant.java
+++ sky-common/src/main/java/com/sky/constant/MessageConstant.java
@@ -5,6 +5,8 @@ package com.sky.constant;
  */
 public class MessageConstant {
 
+    public static final String ALREADY_EXISTS = "already exists";
+
     public static final String PASSWORD_ERROR = "密码错误";
     public static final String ACCOUNT_NOT_FOUND = "账号不存在";
     public static final String ACCOUNT_LOCKED = "账号被锁定";
